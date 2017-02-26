3145f7c1bea3459e0ff4c7d69658cd3f2d7ae3a9
Sun <sunofsparda@gmail.com>
2017-02-26 23:41:46 +0300

    Update provision_jenkins_slave.sh

--- install/jenkins/provision_jenkins_slave.sh
+++ install/jenkins/provision_jenkins_slave.sh
@@ -9,8 +9,8 @@ yum -y update
 
 ### install rpm from local vagrant share folder
 cd /vagrant/soft
-
-yum -y localinstall /vagrant/soft/jdk-8u121-linux-x64.rpm /vagrant/soft/mc-4.8.7-11.el7.x86_64.rpm /vagrant/soft/net-tools-2.0-0.17.20131004git.el7.x86_64.rpm /vagrant/soft/vim-common-7.4.160-1.el7.x86_64.rpm
+#yum -y localinstall /vagrant/soft/jdk-8u121-linux-x64.rpm /vagrant/soft/mc-4.8.7-11.el7.x86_64.rpm /vagrant/soft/net-tools-2.0-0.17.20131004git.el7.x86_64.rpm /vagrant/soft/vim-common-7.4.160-1.el7.x86_64.rpm
+yum -y --exclude=jenkins-2.32.2-1.1.noarch.rpm localinstall *
 
 ### add rules for 8080
 firewall-cmd --add-port=8080/tcp --zone=public --permanent
